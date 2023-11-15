8424ddf95c11540643585663dd9b69386ccdafc9
hasib321 <hasibujjaman123@gmail.com>
2023-11-15 15:37:49 +0600

    booking delete updated

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ async function run() {
       );
       res.send(result);
     });
-
+    //booking delete
     app.delete("/booking/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
